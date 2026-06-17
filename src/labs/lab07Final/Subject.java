0f9fc4612cc8f98a79935de29983380ab38c1b4a
jms1133 <cacao38@gmail.com>
2026-06-17 10:25:53 +0200

    final Lab

---
+++ src/labs/lab07Final/Subject.java
@@ -0,0 +1,26 @@
+package labs.lab07Final;
+
+public class Subject {
+    String id;
+    String name;
+    public Subject(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public String toString(){
+        return id + " " + name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+
+    }
+}
+
